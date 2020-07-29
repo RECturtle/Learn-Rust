a2dc6963f5e9ea436229bfc096d9407fc0f7e4f3
recturtle <spencer.mertes@gmail.com>
2020-07-29 09:07:46 -0500

    Finished chapter 6

---
+++ coin_enums/src/main.rs
@@ -0,0 +1,67 @@
+// enum for US states that will be utilized by Coin::Quarter
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    Arkansas,
+}
+
+// enum for coins
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // the _ is a catch all so anything can be passed to this match and it
+    // won't throw a non-exhaustive pattern error
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
+    // if let is like match for simple use cases so the _ => () is not needed
+    // Tradeoff is that it's not exhaustive like match 
+    let mut count = 0;
+    let coin = Coin::Penny;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
