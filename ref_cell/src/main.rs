b3df1f1bb13ce2d0198c4e12077a30cf67c1804b
recturtle <spencer.mertes@gmail.com>
2020-08-14 09:44:16 -0500

    finished chpt 15

---
+++ ref_cell/src/main.rs
@@ -0,0 +1,6 @@
+// Create a library the tracks a value against a max value and sends messages
+// back on how close we are to the max the current value is
+
+fn main() {
+    println!("Hello, world!");
+}
