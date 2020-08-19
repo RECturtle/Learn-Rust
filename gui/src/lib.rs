4418b55217a501bf7b5800321bdf319d31126fa6
recturtle <spencer.mertes@gmail.com>
2020-08-19 14:32:39 -0500

    17.1 and 17.2

---
+++ gui/src/lib.rs
@@ -0,0 +1,30 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        println!("Drew a button");
+        println!("Button width = {}", self.width);
+        println!("Button height = {}", self.height);
+        println!("Button label = {}", self.label);
+    }
+}
